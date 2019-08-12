dd55210fe221ded59f9562bdd2983100330435b3
Win <synwe@yahoo.com>
2019-08-12 13:34:18 +0000

    Update example

--- example/main.dart
+++ example/main.dart
@@ -1,12 +1,26 @@
 import 'package:traccar_client/traccar_client.dart';
+import 'package:pedantic/pedantic.dart';
 import 'conf.dart';
 
-main() async {
-  var updater = DevicesPositions(serverUrl: serverUrl, userToken: userToken);
-  var positions = await updater.positions();
+void main() async {
+  final trac = DevicesPositions(
+      serverUrl: serverUrl, userToken: userToken, verbose: true);
+  unawaited(trac.init());
+  await trac.onReady;
+
+  /// listen for updates
+  final positions = await trac.positions();
+  print("Listening for position updates");
   positions.listen((device) {
     print("POSITION UPDATE");
-    print("${device.name}: ${device.position.point.latitude} / " +
-        "${device.position.point.longitude}");
+    print("${device.name}: ${device.position.geoPoint.latitude} / " +
+        "${device.position.geoPoint.longitude}");
   });
+
+  /// query for last positions for a device
+  final devices = await trac.queries.positions(
+      deviceId: "2", from: "2019-08-01T18:30:00Z", to: "2019-08-12T13:30:00Z");
+  for (final device in devices) {
+    print(device);
+  }
 }
